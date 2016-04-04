1b3e3373239d7c8a4fa38b8f2e4d47f7785bdf05
Guan Hui <guan.hui@mydreamplus.com>
2016-04-04 18:26:52 +0800

    Initial Commit

---
+++ cardemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  cardemo
+//
+//  Created by MXJ on 4/4/16.
+//  Copyright © 2016 guanhui. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ cardemo/GameViewController.h
@@ -0,0 +1,14 @@
+//
+//  GameViewController.h
+//  cardemo
+//
+
+//  Copyright (c) 2016 guanhui. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <SceneKit/SceneKit.h>
+
+@interface GameViewController : UIViewController
+
+@end
